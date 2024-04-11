882a8fe28181f2ee77d50d584d5b6005deb45d7d
Dario <orquera10@gmail.com>
2024-04-11 12:40:24 -0300

    agregando paginacion falsa

--- src/dao/dbManager/turnos.manager.js
+++ src/dao/dbManager/turnos.manager.js
@@ -1,31 +1,38 @@
 import { Reserva } from "./models/turnos.model.js";
 
 export default class Reservas {
-    constructor() {
-        console.log('Working Turnos with DB in mongoDB');
-    }
-    getReservas = async () => {
-        const result = await Reserva.find().lean();
-        return result;
-    }
+  constructor() {
+    console.log("Working Turnos with DB in mongoDB");
+  }
+  getReservas = async () => {
+    const result = await Reserva.find().lean();
+    return result;
+  };
 
-    addReserva = async (reserva) => {
-        const result = await Reserva.create(reserva);
-        return result;
-    }
+  getReservasPorMes = async (startDate) => {
+    const result = await Reserva.find({
+      hora_entrada: { $gte: startDate }, // Filtrar por eventos del último mes (hora de entrada mayor o igual a la fecha de inicio del último mes)
+    }).lean();
+    return result;
+  };
 
-    getReservaById = async (id) =>{
-        const result = await Reserva.findOne({_id:id}).lean();
-        return result;
-    }
-    
-    deleteReserva = async (id) => {
-        const result = await Reserva.deleteOne({_id:id});
-        return result;
-    }
+  addReserva = async (reserva) => {
+    const result = await Reserva.create(reserva);
+    return result;
+  };
 
-    updateReserva = async (id, reserva) =>{
-        const result = await Reserva.updateOne({_id:id}, reserva);
-        return result;
-    }
+  getReservaById = async (id) => {
+    const result = await Reserva.findOne({ _id: id }).lean();
+    return result;
+  };
+
+  deleteReserva = async (id) => {
+    const result = await Reserva.deleteOne({ _id: id });
+    return result;
+  };
+
+  updateReserva = async (id, reserva) => {
+    const result = await Reserva.updateOne({ _id: id }, reserva);
+    return result;
+  };
 }
--- src/routes/turnos.router.js
+++ src/routes/turnos.router.js
@@ -1,148 +1,172 @@
-import { Router } from 'express';
-import { Productos, Turnos, Detalles, Canchas } from '../dao/factory.js';
-import moment from 'moment';
+import { Router } from "express";
+import { Productos, Turnos, Detalles, Canchas } from "../dao/factory.js";
+import moment from "moment";
 
 const router = Router();
 
-router.get('/', async (req, res) => {
-    const turnosCargados = await Turnos.getReservas();
-    res.json(turnosCargados);
+router.get("/", async (req, res) => {
+  const turnosCargados = await Turnos.getReservas();
+  res.json(turnosCargados);
 });
 
-router.get('/:id', async (req, res) => {
-    const turnoId = req.params.id;
-    const turno = await Turnos.getReservaById(turnoId);
-    res.json(turno);
+//me trae los turnos de los ultimos 2 meses
+router.get("/mes", async (req, res) => {
+  const startDate = new Date();
+  startDate.setMonth(startDate.getMonth() - 2);
+  const turnosMes = await Turnos.getReservasPorMes(startDate);
+  res.json(turnosMes);
 });
 
-router.post('/', async (req, res) => {
-    const { nombre_reserva, hora_entrada, hora_salida, cancha } = req.body;
-    const nuevoDetalle = await Detalles.addDetalle({});
-
-    const canchaElegida = await Canchas.getCanchaById(cancha);
-
-    // Convertir las cadenas de fecha a objetos Moment
-    const nueva_hora_entrada = moment.utc(hora_entrada);
-    const nueva_hora_salida = moment.utc(hora_salida);
-    // Calcular la diferencia en horas
-    const diferencia_en_horas = nueva_hora_salida.diff(nueva_hora_entrada, 'hours');
-
-    const total_alquiler = diferencia_en_horas * canchaElegida.precio_alquiler;
-
-    await Detalles.updateDetalle(nuevoDetalle._id, {total_alquiler: total_alquiler});
-
-    const nuevoTurno = {
-        nombre_reserva,
-        hora_entrada,
-        hora_salida,
-        cancha,
-        detalle: nuevoDetalle._id,
-        evento_terminado: false
-    };
-    // Agregar el nuevo cancha al array de eventos
-    const result = await Turnos.addReserva(nuevoTurno);
-
-    // Responder con el nuevo cancha agregado
-    res.status(200).json(result);
+router.get("/:id", async (req, res) => {
+  const turnoId = req.params.id;
+  const turno = await Turnos.getReservaById(turnoId);
+  res.json(turno);
 });
 
-router.put('/:id', async (req, res) => {
-    const turno = req.body;
-    const turnoId = req.params.id;
-    // Agregar el nuevo cancha al array de eventos
-    const result = await Turnos.updateReserva(turnoId, turno);
+router.post("/", async (req, res) => {
+  const { nombre_reserva, hora_entrada, hora_salida, cancha } = req.body;
+  const nuevoDetalle = await Detalles.addDetalle({});
 
-    // Responder con el nuevo cancha agregado
-    res.status(200).json(result);
+  const canchaElegida = await Canchas.getCanchaById(cancha);
+
+  // Convertir las cadenas de fecha a objetos Moment
+  const nueva_hora_entrada = moment.utc(hora_entrada);
+  const nueva_hora_salida = moment.utc(hora_salida);
+  // Calcular la diferencia en horas
+  const diferencia_en_horas = nueva_hora_salida.diff(
+    nueva_hora_entrada,
+    "hours"
+  );
+
+  const total_alquiler = diferencia_en_horas * canchaElegida.precio_alquiler;
+
+  await Detalles.updateDetalle(nuevoDetalle._id, {
+    total_alquiler: total_alquiler,
+  });
+
+  const nuevoTurno = {
+    nombre_reserva,
+    hora_entrada,
+    hora_salida,
+    cancha,
+    detalle: nuevoDetalle._id,
+    evento_terminado: false,
+  };
+  // Agregar el nuevo cancha al array de eventos
+  const result = await Turnos.addReserva(nuevoTurno);
+
+  // Responder con el nuevo cancha agregado
+  res.status(200).json(result);
 });
 
-router.delete('/:id', async (req, res) => {
-    const turnoId = req.params.id;
-    const turno = await Turnos.getReservaById(turnoId);
-    await Detalles.deleteDetalle(turno.detalle._id);
+router.put("/:id", async (req, res) => {
+  const turno = req.body;
+  const turnoId = req.params.id;
+  // Agregar el nuevo cancha al array de eventos
+  const result = await Turnos.updateReserva(turnoId, turno);
 
-    const result = await Turnos.deleteReserva(turnoId);
-    res.json(result);
+  // Responder con el nuevo cancha agregado
+  res.status(200).json(result);
+});
+
+router.delete("/:id", async (req, res) => {
+  const turnoId = req.params.id;
+  const turno = await Turnos.getReservaById(turnoId);
+  await Detalles.deleteDetalle(turno.detalle._id);
+
+  const result = await Turnos.deleteReserva(turnoId);
+  res.json(result);
 });
 
 //agregar producto a turno
-router.put('/:did/producto/:pid', async (req, res) => {
-    const turnoId = req.params.did;
-    const productoId = req.params.pid;
-    const { cantidad = 1 } = req.body;
+router.put("/:did/producto/:pid", async (req, res) => {
+  const turnoId = req.params.did;
+  const productoId = req.params.pid;
+  const { cantidad = 1 } = req.body;
 
-    const turno = await Turnos.getReservaById(turnoId);
+  const turno = await Turnos.getReservaById(turnoId);
+  const producto = await Productos.getProductoById(productoId);
+
+  const precio_producto = producto.precio_unitario;
+
+  const productoEncontrado = turno.detalle.productos_consumidos.findIndex(
+    (item) => item.producto._id.toString() === productoId
+  );
+
+  let productos_nuevos;
+  let total_consumido;
+
+  if (productoEncontrado !== -1) {
+    productos_nuevos = turno.detalle.productos_consumidos;
+    productos_nuevos[productoEncontrado].cantidad += cantidad;
+    total_consumido =
+      turno.detalle.total_consumido + cantidad * precio_producto;
+  } else {
+    productos_nuevos = turno.detalle.productos_consumidos;
+    productos_nuevos.push({ producto: productoId, cantidad: cantidad });
+    total_consumido =
+      turno.detalle.total_consumido + cantidad * precio_producto;
+  }
+
+  const total = turno.detalle.total_alquiler + total_consumido;
+
+  const detalleNuevo = {
+    productos_consumidos: productos_nuevos,
+    total_alquiler: turno.detalle.total_alquiler,
+    total_consumido: total_consumido,
+    total: total,
+  };
+
+  const result = await Detalles.updateDetalle(turno.detalle._id, detalleNuevo);
+  res.json(result);
+});
+
+router.delete("/:did/producto/:pid", async (req, res) => {
+  const turnoId = req.params.did;
+  const productoId = req.params.pid;
+
+  const turno = await Turnos.getReservaById(turnoId);
+
+  // Verificar si el producto está en el detalle del turno
+  const indiceProducto = turno.detalle.productos_consumidos.findIndex(
+    (item) => item.producto._id.toString() === productoId
+  );
+
+  if (indiceProducto !== -1) {
     const producto = await Productos.getProductoById(productoId);
+    const totalProducto =
+      turno.detalle.productos_consumidos[indiceProducto].cantidad *
+      producto.precio_unitario;
+    const total_consumido = turno.detalle.total_consumido - totalProducto;
+    const total = turno.detalle.total - totalProducto;
 
-    
+    // Si el producto está en el detalle, eliminarlo
+    turno.detalle.productos_consumidos.splice(indiceProducto, 1);
 
-    const precio_producto = producto.precio_unitario;
+    // Actualizar el detalle del turno
+    const detalleActualizado = {
+      productos_consumidos: turno.detalle.productos_consumidos,
+      total_alquiler: turno.detalle.total_alquiler,
+      total_consumido: total_consumido,
+      total: total,
+    };
 
-    const productoEncontrado = turno.detalle.productos_consumidos.findIndex(item => item.producto._id.toString() === productoId);
-    
-    let productos_nuevos
-    let total_consumido
+    await Detalles.updateDetalle(turno.detalle._id, detalleActualizado);
 
-    if (productoEncontrado !== -1) {
-        productos_nuevos = turno.detalle.productos_consumidos;
-        productos_nuevos[productoEncontrado].cantidad += cantidad;
-        total_consumido = turno.detalle.total_consumido + cantidad * precio_producto;
-    } else {
-        productos_nuevos = turno.detalle.productos_consumidos;
-        productos_nuevos.push({ producto: productoId, cantidad: cantidad });
-        total_consumido = turno.detalle.total_consumido + cantidad * precio_producto;
-    }
-
-    
-    const total = turno.detalle.total_alquiler + total_consumido;
-
-    const detalleNuevo = {
-        productos_consumidos: productos_nuevos,
-        total_alquiler: turno.detalle.total_alquiler,
-        total_consumido: total_consumido,
-        total: total
-    }
-
-    const result = await Detalles.updateDetalle(turno.detalle._id, detalleNuevo);
-    res.json(result);
+    // Responder con éxito
+    res
+      .status(200)
+      .json({
+        message: "Producto eliminado del detalle del turno exitosamente.",
+      });
+  } else {
+    // Si el producto no está en el detalle, responder con un mensaje de error
+    res
+      .status(404)
+      .json({
+        message: "El producto no está presente en el detalle del turno.",
+      });
+  }
 });
-
-router.delete('/:did/producto/:pid', async (req, res) => {
-    const turnoId = req.params.did;
-    const productoId = req.params.pid;
-
-    const turno = await Turnos.getReservaById(turnoId);
-
-    // Verificar si el producto está en el detalle del turno
-    const indiceProducto = turno.detalle.productos_consumidos.findIndex(item => item.producto._id.toString() === productoId);
-
-    if (indiceProducto !== -1) {
-
-        const producto = await Productos.getProductoById(productoId);
-        const totalProducto = turno.detalle.productos_consumidos[indiceProducto].cantidad * producto.precio_unitario;
-        const total_consumido = turno.detalle.total_consumido - totalProducto;
-        const total = turno.detalle.total - totalProducto;
-
-        // Si el producto está en el detalle, eliminarlo
-        turno.detalle.productos_consumidos.splice(indiceProducto, 1);
-
-        // Actualizar el detalle del turno
-        const detalleActualizado = {
-            productos_consumidos: turno.detalle.productos_consumidos,
-            total_alquiler: turno.detalle.total_alquiler,
-            total_consumido: total_consumido,
-            total: total
-        };
-
-        await Detalles.updateDetalle(turno.detalle._id, detalleActualizado);
-
-        // Responder con éxito
-        res.status(200).json({ message: 'Producto eliminado del detalle del turno exitosamente.' });
-    } else {
-        // Si el producto no está en el detalle, responder con un mensaje de error
-        res.status(404).json({ message: 'El producto no está presente en el detalle del turno.' });
-    }
-});
-
 
 export default router;
